f0667a3644209e2341ee3195bf3ba7bfe87589cf
Jared Lumpe <mjlumpe@gmail.com>
2017-12-20 13:09:28 -0700

    Empty package structure and install script

---
+++ pydatatypes/__init__.py
@@ -0,0 +1,3 @@
+"""Work with pseudo-statically-typed data structures."""
+
+from .version import __version__
---
+++ setup.py
@@ -0,0 +1,23 @@
+"""setuptools installation script for pydatatypes package."""
+
+from setuptools import setup, find_packages
+from distutils.util import convert_path
+
+
+# Get package version without importing it
+version_ns = dict()
+with open(convert_path('pydatatypes/version.py')) as fobj:
+	exec(fobj.read(), version_ns)
+version = version_ns['__version__']
+
+
+setup(
+	name='pydatatypes',
+	version=version,
+	description='Package for working with pseudo-statically-typed data structures',
+	author='Jared Lumpe',
+	author_email='mjlumpe@gmail.com',
+	packages=find_packages(),
+	install_requires=[
+	],
+)
