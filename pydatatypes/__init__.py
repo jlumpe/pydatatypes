98d9682d3a000e2faa97105865ded6ee24f5bfc7
Jared Lumpe <mjlumpe@gmail.com>
2017-12-20 15:02:43 -0700

    Import all at top-level package

--- pydatatypes/__init__.py
+++ pydatatypes/__init__.py
@@ -1,3 +1,8 @@
 """Work with pseudo-statically-typed data structures."""
 
 from .version import __version__
+
+from .abc import *
+from .typing import *
+from .json import *
+from .dataclass import *
